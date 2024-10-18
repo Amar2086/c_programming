6cd76291c8d7ec9f9046b2682888ab5cd8d65c81
Amar2086 <omar2086pcc40sh@gmail.com>
2024-10-18 10:27:48 +0600

    first commit

--- array/max_min.c
+++ array/max_min.c
@@ -4,22 +4,28 @@
 int main()
 {
      int n, array[100],max,min;
+    
      printf("Enter the number of elements: ");
      scanf("%d",&n);
      printf("Enter %d integers: ",n);
      for(int i=0; i<n; i++)
      {
         scanf("%d",&array[i]);
+         
      }
-     max = array[0];
+      max = array[0];
      min = array[0];
-     for(int i=1; i<n; i++)
+     for (int i = 0; i < n; i++)
      {
-        if(array[i] > max)
+        /* code */
+         if(array[i] > max)
             max = array[i];
-        if(array[i] < min)
+       if(array[i]<min)
             min = array[i];
      }
+     
+     
+    
      printf("Maximum: %d\n",max);
      printf("Minimum: %d\n",min);
      
--- run.c
+++ run.c
@@ -1,8 +1,30 @@
-// Trying to dereferencing a null pointer
+#include<conio.h>
 #include<stdio.h>
+
 int main()
 {
-int * ptr = NULL;
-printf("%d",*ptr);
-return 0;
+     int n, array[100],max,min;
+    
+     printf("Enter the number of elements: ");
+     scanf("%d",&n);
+     printf("Enter %d integers: ",n);
+     for(int i=0; i<n; i++)
+     {
+        scanf("%d",&array[i]);
+         max = array[0];
+     min = array[0];
+            if(array[i] > max)
+            max = array[i];
+       if(array[i]<min)
+            min = array[i];
+     }
+     
+     
+     
+     
+    
+     printf("Maximum: %d\n",max);
+     printf("Minimum: %d\n",min);
+     
+ return 0;
 }
