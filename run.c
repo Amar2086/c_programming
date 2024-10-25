a370357d00a326686a27ae5f5e27898617e7473d
Amar2086 <omar2086pcc40sh@gmail.com>
2024-10-25 14:33:38 +0600

    first commit

--- run.c
+++ run.c
@@ -1,25 +1,16 @@
-#include <stdio.h>
+#include<stdio.h>
+#pragma pack(1)
 
-// Define a structure called "Person"
-struct Person {
-    char name[50];
-    int age;
-    float height;
-};
+typedef struct untity
+{
+    double i;
+    char c;
+    int j;
+}wow;
 
-int main() {
-    // Declare a variable of type "struct Person"
-    struct Person person1;
+int main()
+{
+     printf("%d",sizeof(wow));
 
-    // Assign values to the members of "person1"
-    strcpy(person1.name, "Alice");  // Copy string into the name field
-    person1.age = 25;
-    person1.height = 5.6;
-
-    // Print the values
-    printf("Name: %s\n", person1.name);
-    printf("Age: %d\n", person1.age);
-    printf("Height: %.1f\n", person1.height);
-
-    return 0;
+ return 0;
 }
--- run2.c
+++ run2.c
@@ -1,20 +1,30 @@
-// A simple C program to show function pointers as parameter
-#include <stdio.h>
-
-// Two simple functions
-void fun1() { printf("Fun1\n"); }
-void fun2() { printf("Fun2\n"); }
-
-// A function that receives a simple function
-// as parameter and calls the function
-void wrapper(void (*fun)())
- {
-     fun(); 
- }
+#include<stdio.h>
 
 int main()
 {
-    wrapper(fun1);
-    wrapper(fun2);
-    return 0;
+     int i;
+
+    printf("Even:");
+     for ( i = 1; i <=10; i++)
+     {
+        /* code */
+        if (i%2==0)
+        {
+            /* code */
+            printf("%d",i);
+        }
+        
+     }
+     printf("\nOdd:");
+     for ( i = 0; i <10; i++)
+     {
+        /* code */
+        if(i%2!=0)
+        printf("%d",i);
+
+     }
+     
+     
+
+ return 0;
 }
